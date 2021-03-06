109f3bbe29af78fb9316eb2982a8e14409d184a0
Muhammad Nasrul <mnasruul@gmail.com>
2021-03-06 11:09:18 +0700

    chage packages respone err

--- jwt_utils/jwt_utils.go
+++ jwt_utils/jwt_utils.go
@@ -1,12 +1,20 @@
 package jwt_utils
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/mnasruul/go-utils/rest_errors"
+)
 
-func GetToken(input interface{},secretKey string) (string, error)  {
+func GetToken(input interface{},secretKey string) (response *string,err rest_errors.RestErr)  {
 	signingKey := []byte(secretKey)
- 	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{"claims":input})
- 	tokenString, err := token.SignedString(signingKey)
-	return tokenString, err
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"claims_value":input,
+	})
+	tokenString, errT := token.SignedString(signingKey)
+	if errT != nil {
+		return nil, rest_errors.NewInternalServerError("Error generating JWT token " ,errT)
+	}
+	return &tokenString, nil
 }
 
 func verifyToken(tokenString string, secretKey string) (jwt.Claims, error) {
